f65fad5d8c42a28a2aeb00740876797b44d07e14
triptotest01 <triptotest01@gmail.com>
2023-05-07 13:36:46 +0900

    useState, useEffect(), fetch url 디비통신/ 커스텀useFetch

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,21 +1,35 @@
-import dummy from "../db/data.json"
 import { useParams } from "react-router-dom";
 import Word from "./Word";
+import { useEffect, useState } from "react";
+import useFetch from "../hooks/useFetch";
 
 export default function Day(){
 
   // const inputDay=3;
   const {inputDay} =useParams();
 
+  // const [words, setWords] =useState([]);
+
+  // useEffect( ()=>{
+  //   // fetch("http://localhost:4000/words")
+  //   // fetch("http://localhost:4000/words?day=1")
+  //   fetch(`http://localhost:4000/words?day=${inputDay}`)
+  //     .then(res => {return res.json();})
+  //     .then(data=>{setWords(data);});
+  //  } , []
+  // )
+
+  const words = useFetch(`http://localhost:4000/words?day=${inputDay}`)
+
   // Number 는 문자를 숫자로 변환해줌  (url주소에 들어온 숫자는 문자로인식!)
-  const wordList = dummy.words.filter(word => (word.day===Number(inputDay)))
+  // const wordList = dummy.words.filter(word => (word.day===Number(inputDay)))
 // wordList : filter함수로 day가3인 요소들을 담은 배열집합 (words배열집합을 filter재가공한 또다른 배열집합)
  return(
   <>
   <h2> 오늘의 학습 : Day {inputDay} </h2>
     <table>
       <tbody>
-        {wordList.map(word =>(
+        {words.map(word =>(
           <Word word={word} key={word.id}></Word>
         ))}
       </tbody>
--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,19 +1,26 @@
+// Day.js와 DayList.js에서 useState와 useEffect의 반복사용을 피하기위해 
+// 해당함수 2개는 커스텀훅스인 useFetch에 한번 작성하고 호출하여사용
+
 import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+import useFetch from "../hooks/useFetch";
+
 
 export default function DayList(){
-  const[days, setDays] =useState([])
 
+  // const[days, setDays] =useState([])
 
-  // useEffect(함수, []) --> 초기 화면 렌더링이후 한번만 실행! 
-  // 즉, 렌더링이후,  json 데이터를 한번 가져옴 
-  useEffect(()=>{
-    fetch("http://localhost:4000/days")
-    .then(res => {return res.json();})
-    .then(data => {setDays(data);})
-  },[]);
+  // // useEffect(함수, []) --> 초기 화면 렌더링이후 한번만 실행! 
+  // // 즉, 렌더링이후,  json 데이터를 한번 가져옴 
+  // useEffect(()=>{
+  //   fetch("http://localhost:4000/days")
+  //   .then(res => {return res.json();})
+  //   .then(data => {setDays(data);})
+  // },[]);
 
-
+  // 함수호출
+  const days = useFetch("http://localhost:4000/days")
+  
   return(
     <ul className="list_day">
         {days.map( day =>(
